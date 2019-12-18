facca020f3a100d1426accf86b8a33eb5f7aa435
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-12-18 15:16:24 -0600

    Registro de solicitud

--- html/Views/headerContent/menuVisitante.php
+++ html/Views/headerContent/menuVisitante.php
@@ -1,34 +1,21 @@
-              <div class="nav-collapse collapse">
-                <div class="menu-menu-principal-container">
+          <div class="nav-collapse collapse">
+            <div class="menu-menu-principal-container">
                   <ul id="menu-menu-principal-2" class="nav">
+
                     <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170">
                       <a href="indexFree.php">
                         Inicio
                       </a>
                     </li>
-                    
-                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170" >
-                      <a  href="../Views/login.php" class="trigger-btn" data-toggle="modal">
-			                  <div class="nav-collapse collapse">
-                          <div class="menu-menu-principal-container">
-                            <ul id="menu-menu-principal-2" class="nav-Personalizada">
-                  <li class="nav-item" >
-                      <a class="nav-link active" href="indexFree.php">
-                        Inicio
-                      </a>
-                    </li>
-                    <li class="nav-item" >
-                      <a class="nav-link trigger-btn" href="#iniciarSesion"  data-toggle="modal">
 
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page dropdown menu-item-385" >
+                      <a href="login.php" >
                         Iniciar sesi&oacute;n
                       </a>
                     </li>
 
-                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170" >
-                      <a  href="../Views/registrarseVisitante.php" class="trigger-btn"  data-toggle="modal">
-
-                    <li class="nav-item" >
-                      <a class="nav-link" href="#">
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page dropdown menu-item-385" >
+                      <a href="registrarseVisitante.php" title="Usuario">
                         Registrarse
                       </a>
                     </li>
