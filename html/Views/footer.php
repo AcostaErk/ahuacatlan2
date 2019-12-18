5e3abd77b809960d3a6518ea54207e9e1bce4386
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-12-17 18:44:31 -0600

    Footer separado del body IndexFree

---
+++ html/Views/footer.php
@@ -0,0 +1,234 @@
+<!-- FOOTER -->
+<footer id="footer" class="">
+  <div class="container footer-widgets ">
+    <div class="row widget-area" role="complementary">
+      <div id="footer_one" class="span4">
+        <aside id="wpcw_contact-8" class="widget wpcw-widgets wpcw-widget-contact">
+          <h3 class="widget-title">
+            Presidencia Municipal
+          </h3>
+          
+          <ul>
+            <li class="has-label">
+                      <strong>
+                        Email
+                      </strong>
+                      <br>
+                      <div>
+                        <a href="mailto:pr&#101;&#115;&#105;&#100;&#101;ncia&#64;&#97;hu&#97;&#99;a&#116;&#108;an&#97;&#118;a&#110;&#122;&#97;&#46;&#103;ob&#46;mx">
+                            pr&#101;&#115;&#105;&#100;&#101;ncia&#64;&#97;hu&#97;&#99;a&#116;&#108;an&#97;&#118;a&#110;&#122;&#97;&#46;&#103;ob&#46;mx
+                        </a>
+                      </div>
+                    </li>
+                    <li class="has-label">
+                      <strong>
+                        Teléfono
+                      </strong>
+                      <br>
+                      <div>
+                        (324)241 1254
+                      </div>
+                    </li>
+                    <li class="has-label">
+                      <strong>
+                        Dirección
+                      </strong>                         
+                      <br>
+                      <div>
+                        Calle Hidalgo s/n, Centro, 63900                          
+                      <br/>
+                Ahuacatlán, Nayarit
+              </div>
+            </li>
+          </ul>
+        </aside>
+            </div><!-- .{$key}_widget_class -->
+      <div id="footer_two" class="span4">
+                <aside id="nav_menu-5" class="widget widget_nav_menu">
+                  <h3 class="widget-title">
+                    Navegacion
+                  </h3>
+                  <div class="menu-menu-principal-container">
+                    <ul id="menu-menu-principal-3" class="menu">
+                      <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-172">
+                        <a href="http://ahuacatlanavanza.gob.mx/cabildo/">
+                          Cabildo
+                        </a>
+                <ul class="sub-menu">
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2226">
+                      <a href="http://ahuacatlanavanza.gob.mx/gacetas-municipales/">
+                        Gaceta Oficial
+                      </a>
+                    </li>
+                </ul>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170">
+                <a href="http://ahuacatlanavanza.gob.mx/inicio/">
+                  Inicio
+                </a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-327">
+                <a href="http://ahuacatlanavanza.gob.mx/te-escuchamos/">
+                  Queremos Escucharte
+                </a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-383">
+                <a href="http://ahuacatlanavanza.gob.mx/comunicacion-social/">
+                  Comunicación Social
+                </a>
+                <ul class="sub-menu">
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2261">
+                      <a href="http://ahuacatlanavanza.gob.mx/licitaciones-publicas/">
+                        Convocatorias y Licitaciones Públicas
+                      </a>
+                    </li>
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2793">
+                      <a href="http://ahuacatlanavanza.gob.mx/quejas-y-denuncias/">
+                        Quejas y Denuncias
+                      </a>
+                    </li>
+                </ul>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-384">
+                <a href="http://ahuacatlanavanza.gob.mx/tramites/">
+                  Trámites
+                </a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-385">
+                <a href="http://ahuacatlanavanza.gob.mx/transparencia/">
+                  transparencia
+                </a>
+                <ul class="sub-menu">
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1005">
+                      <a href="http://ahuacatlanavanza.gob.mx/ayuntamientoitai/">
+                        Ayuntamiento
+                      </a>
+                      <ul class="sub-menu">
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1002">
+                          <a href="http://ahuacatlanavanza.gob.mx/avisos-de-privacidad/">
+                            Avisos de Privacidad
+                          </a>
+                        </li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1001">
+                          <a href="http://ahuacatlanavanza.gob.mx/articulo-10-historico/">
+                            Articulo 10 (Historico)
+                          </a>
+                        </li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-395">
+                          <a href="http://ahuacatlanavanza.gob.mx/articulo33/">
+                            Articulo 33
+                          </a>
+                        </li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-394">
+                          <a href="http://ahuacatlanavanza.gob.mx/articulo39/">
+                            Articulo 39
+                          </a>
+                        </li>
+                    </ul>
+                  </li>
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1009">
+                      <a href="http://ahuacatlanavanza.gob.mx/organismos-descentralizados/">
+                        Organismos Descentralizados
+                      </a>
+                      <ul class="sub-menu">
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-397">
+                          <a href="http://ahuacatlanavanza.gob.mx/dif/">
+                            DIF
+                          </a>
+                        </li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6790">
+                          <a href="http://ahuacatlanavanza.gob.mx/ooapa-2/">
+                            OOAPA
+                          </a>
+                        </li>
+                      </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </aside>
+          </div><!-- .{$key}_widget_class -->
+
+            
+            <div id="footer_three" class="span4">
+                <aside id="search-6" class="widget widget_search">
+                  <h3 class="widget-title">
+                    Busqueda
+                  </h3>
+                  <form role="search" method="get" id="searchform" class="searchform" action="http://ahuacatlanavanza.gob.mx/">
+              <div>
+                    <label class="screen-reader-text" for="s">Buscar:</label>
+                    <input type="text" value="" name="s" id="s" />
+                  <input type="submit" id="searchsubmit" value="Buscar" />
+                </div>
+              </form>
+            </aside>
+            </div><!-- .{$key}_widget_class -->
+    </div><!-- .row.widget-area -->
+    </div><!--.footer-widgets -->
+    <div class="colophon">
+        <div class="container">
+            <div class="row-fluid">
+              <div class="span3 social-block pull-left">
+                <span class="social-links">
+                  <a rel="nofollow" class="social-icon icon-facebook"  title="Síguenos Facebook" aria-label="Síguenos Facebook" href="https://www.facebook.com/XLIAyuntamientoAhuacatlan/"  target="_blank"  style="color:#f51818;font-size:12px">
+                    <i class="fa fa-facebook">
+                    </i>
+                  </a>
+                  <a rel="nofollow" class="social-icon icon-twitter"  title="Síguenos Twitter" aria-label="Síguenos Twitter" href="javascript:void(0)"  target="_blank"  style="color:#f51818;font-size:12px">
+                    <i class="fa fa-twitter">
+                    </i>
+                  </a>
+                  <a rel="nofollow" class="social-icon icon-youtube"  title="Síguenos Youtube" aria-label="Síguenos Youtube" href="javascript:void(0)"  target="_blank"  style="color:#f51818;font-size:12px">
+                    <i class="fa fa-youtube">
+                      
+                    </i>
+                  </a>
+                  <a rel="nofollow" class="social-icon icon-instagram"  title="Síguenos Instagram" aria-label="Síguenos Instagram" href="javascript:void(0)"  target="_blank"  style="color:#f51818;font-size:12px">
+                    <i class="fa fa-instagram">
+                    
+                    </i>
+                </a>
+                <a rel="nofollow" class="social-icon icon-phone"  title="(324)2411254" aria-label="(324)2411254" href="tel:324-241-1254"  target="_blank"  style="color:#f51818;font-size:12px">
+                  <i class="fa fa-phone">
+                  
+                  </i>
+                </a>
+                <a rel="nofollow" class="social-icon icon-mail-o"  title="Email" aria-label="Email" href="mailto:ahuacatlanavanza@gmail.com"  target="_blank"  style="color:#f51818;font-size:12px">
+                  <i class="fa fa-envelope-o">
+                  
+                  </i>
+                </a>
+            </span>
+          </div>
+          <div class="span6 credits">
+            <p>
+              &middot; 
+              <span class="tc-copyright-text">
+              &copy; 2019
+              </span> 
+              <a href="http://ahuacatlanavanza.gob.mx" title="XLI Ayuntamiento de Ahuacatlán Nayarit " rel="bookmark">
+                XLI Ayuntamiento de Ahuacatlán Nayarit 
+              </a> 
+              &middot; 
+              <span class="tc-wp-powered-text">
+                Creado con
+              </span> 
+              <a class="icon-wordpress" target="_blank" href="https://wordpress.org" title="Creado con WordPress">
+              
+              </a>
+              &middot; 
+              <span class="tc-credits-text">
+                Diseñado con el 
+                <a class="czr-designer-link" href="https://presscustomizr.com/customizr" title="Tema Customizr">
+                  Tema Customizr
+                </a> 
+              </span>
+              &middot;
+            </p>
+          </div>             
+      </div><!-- .row-fluid -->
+  </div><!-- .container -->
+</div><!-- .colophon -->
+</footer>
---
+++ html/Views/footerContent/script.php
@@ -0,0 +1,10 @@
+	<script type='text/javascript'></script>
+    <script type='text/javascript' src='http://ahuacatlanavanza.gob.mx/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=5.1.4'></script>
+    <script type='text/javascript' src='http://ahuacatlanavanza.gob.mx/wp-content/plugins/smooth-accordion/assets/js/smoothaccordion.js?ver=1.1'></script>
+    <script type='text/javascript' src='http://ahuacatlanavanza.gob.mx/wp-content/plugins/smooth-accordion/assets/js/smoothScript.js?ver=1.1'></script>
+    <script type='text/javascript' src='https://www.google.com/recaptcha/api.js?render=6LcFLYUUAAAAAIdi52qzcnxQcM5Evwj3sDpY5GUj&#038;ver=3.0'></script>
+    <script type='text/javascript' src='http://ahuacatlanavanza.gob.mx/wp-content/themes/customizr/assets/front/js/libs/modernizr.min.js?ver=4.0.13'></script>
+    <script type='text/javascript'></script>
+    <script type='text/javascript' src='http://ahuacatlanavanza.gob.mx/wp-content/plugins/godaddy-email-marketing-sign-up-forms/js/gem.min.js?ver=1.4.2'></script>
+    <script type='text/javascript' src='http://ahuacatlanavanza.gob.mx/wp-includes/js/wp-embed.min.js?ver=5.3.1'></script>
+    <script type="text/javascript"></script>
